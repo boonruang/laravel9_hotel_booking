779f794f5e6331fd4f0bc0d96f4a905d27979d0d
boonruang <scubatoy@gmail.com>
2022-11-14 12:05:24 +0700

    111. Features - Part 2

---
+++ app/Http/Controllers/Admin/AdminFeatureController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Feature;
+
+class AdminFeatureController extends Controller
+{
+    public function index()
+    {
+        $features = Feature::get();
+        return view('admin.feature_view',compact('features'));
+    }
+
+    public function add()
+    {
+        return view('admin.feature_add');
+    }
+
+}
