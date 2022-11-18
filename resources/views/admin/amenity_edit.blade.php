5e015c1f6f95b5a2a9537dbb5684e3e28c14c2a0
boonruang <scubatoy@gmail.com>
2022-11-18 14:13:11 +0700

    159. Room Amenity - Part 2

---
+++ resources/views/admin/amenity_edit.blade.php
@@ -0,0 +1,49 @@
+@extends('admin.layout.app')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+@section('heading','Edit Amenity')
+
+@section('right_top_button')  
+<a href="{{route('admin_amenity_view')}}" class="btn btn-primary"><i class="fa fa-eye"></i> View All</a>
+@endsection
+
+@section('main_content')
+<div class="section-body">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <form action="{{route('admin_amenity_update',$amenity_data->id)}}" method="post">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="mb-4">
+                                    <label class="form-label">Name *</label>
+                                    <input type="text" class="form-control" name="name" value="{{$amenity_data->name}}">
+                                </div>                                
+
+                                <div class="mb-4">
+                                    <label class="form-label"></label>
+                                    <button type="submit" class="btn btn-primary">Update</button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>  
+@endsection
